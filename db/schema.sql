f04e55aff09c1d5abf86b5a501e1934538f2d55f
patel <parthpatel167@gmail.com>
2020-12-05 18:08:47 -0600

    employee tracker database schema and seed updated,And working good

---
+++ db/schema.sql
@@ -0,0 +1,31 @@
+DROP DATABASE IF EXISTS employee_tracker;
+
+CREATE database employee_tracker;
+
+USE employee_tracker;
+
+CREATE TABLE department(
+	id INT NOT NULL auto_increment PRIMARY KEY
+	, title VARCHAR(30)
+  
+);
+
+CREATE TABLE role(
+    id INT NOT NULL auto_increment PRIMARY KEY
+  , title VARCHAR(30)
+  , salary DECIMAL(8, 2)
+  , department_id INT
+  , FOREIGN KEY(department_id) REFERENCES department(id)
+);
+
+
+
+CREATE TABLE employee (
+  id INT NOT NULL auto_increment PRIMARY KEY
+, first_name VARCHAR(30)
+, last_name VARCHAR(30)
+, role_id INT
+, manager_id INT
+, FOREIGN KEY(role_id) REFERENCES role(id)
+, FOREIGN KEY(manager_id) REFERENCES employee(id)
+);
