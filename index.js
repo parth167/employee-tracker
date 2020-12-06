95766a186b7a5f3365db699c01d50d7c605dc08e
patel <parthpatel167@gmail.com>
2020-12-05 21:18:01 -0600

    inquirer question added and all employee table is ready

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const mysql = require("mysql");
-
+const inquirer = require("inquirer");
 
 // create the connection information for the sql database
 const connection = mysql.createConnection({
@@ -22,3 +22,70 @@ connection.connect(function(err) {
   // run the start function after the connection is made to prompt the user
   start();
 });
+
+///inquirer prompts
+function questions() {
+    inquirer
+        .prompt({
+        name: "questionAsked",
+        type: "list",
+        message: "What Would You Like To Do?",
+        choices: [
+            "View All Employees",
+            "View All Employees By Department",
+            "View All Employees By Manager",
+            "Add Employee",
+            "Remove Employee",
+            "Update Employee Role",
+            "Update Employee Manager",
+        ],
+        })
+        .then(function (answer) {
+        switch (answer.questionAsked) {
+            case "View All Employees":
+            allEmployees();
+            break;
+            case "View All Employees By Department":
+            employeesByDepartment();
+            break;
+            case "View All Employees By Manager":
+            employeesByManager();
+            break;
+            case "Add Employee":
+            addEmployees();
+            break;
+            case "Remove Employee":
+            //allEmployees()
+            break;
+            case "Update Employee Role":
+            //allEmployees()
+            break;
+            case "Update Employee Manager":
+            //allEmployees()
+        }
+    });
+}
+ 
+  //view all employees function
+function allEmployees() {
+    connection.query(
+      "SELECT t1.first_name, t1.last_name, t2.title,t3.department, t2.salary FROM employee AS t1 JOIN role AS t2 ON t1.role_id=t2.id JOIN department AS t3 ON t2.department_id=t3.id",
+        function (err, results) {
+          if (err) throw err;
+          console.table(results);
+          
+        }
+      );
+}
+
+//view all employees by Department function
+function employeesByDepartment() {}
+
+//view all employees by Manager function
+function employeesByManager() {}
+//add employees function
+function addEmployees() {}
+function start() {
+  //welcomeScreen();
+  questions();
+}
