e1c9c41100b0f87fa36958b40967e19a8831d9a6
patel <parthpatel167@gmail.com>
2020-12-05 18:22:43 -0600

    connection with sql employee_tracker database

---
+++ index.js
@@ -0,0 +1,24 @@
+const mysql = require("mysql");
+
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "Parth@167",
+  database: "employee_tracker"
+});
+
+// connect to the mysql server and sql database
+connection.connect(function(err) {
+  if (err) throw err;
+  // run the start function after the connection is made to prompt the user
+  start();
+});
