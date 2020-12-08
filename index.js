fae9dcef9462d697fdb42e7735cb8c4c0061e663
patel <parthpatel167@gmail.com>
2020-12-07 21:29:38 -0600

    all connection with lib file working ,mysql connectionis work,every question inquirer

--- index.js
+++ index.js
@@ -1,5 +1,25 @@
 const inquirer = require("inquirer");
 const mysql = require("mysql");
+const figlet = require('figlet');
+
+const {
+  viewAllDept,
+  viewAllRoles,
+  viewAllEmployees,
+  viewAll,
+} = require("./lib/view");
+const { addDept,
+  addRole,
+  addEmployee 
+} = require("./lib/add");
+const {
+  updateEmployeeRole,
+  
+} = require("./lib/update");
+const { deleteDept,
+  deleteRole, 
+  deleteEmployee 
+} = require("./lib/delete");
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -8,50 +28,99 @@ const connection = mysql.createConnection({
   password: "Parth@167",
   database: "employee_tracker",
 });
-
-connection.connect(function (err) {
+// Connect to the mySQL Server & the DB
+connection.connect((err) => {
   if (err) throw err;
-  init();
+  console.log('connected as id ' + connection.threadId);
+  figlett();
+  
 });
-const firstQuestion = {
-  name: "firstQuestion",
-  type: "list",
-  message: "What would you like to do?",
-  choices: [
-    "Add Employee",
-    "Add Department",
-    "Add Role",
-    "View Employees",
-    "View By Departments",
-    "View By Roles",
-    "Update Employee Role",
-  ],
+
+function questions() {
+  inquirer
+    .prompt({
+      name: "question",
+      type: "list",
+      message: "Please select one of the following question.",
+      choices: [
+        "View All",
+        "View All Employees",
+        "View All Roles",
+        "View All Departments",
+        "Add Employee",
+        "Add Role",
+        "Add Department",
+        "Update Employee Role",
+        "Delete Employee",
+        "Delete Role",
+        "Delete Department",
+        "Exit",
+      ],
+    })
+
+    .then((userInput) => {
+      switch (userInput.question) {
+        
+        case "View All":
+          viewAll(connection, start);
+          break;
+        case "View All Employees":
+          viewAllEmployees(connection, start);
+          break;
+        case "View All Roles":
+          viewAllRoles(connection, start);
+          break;
+        case "View All Departments":
+          viewAllDept(connection, start);
+          break;
+        case "Add Employee":
+          addEmployee(connection, start);
+          break;
+        case "Add Role":
+          addRole(connection, start);
+          break;
+        case "Add Department":
+          addDept(connection, start);
+          break;
+        case "Update Employee Role":
+          updateEmployeeRole(connection, start);
+          break;
+        case "Delete Employee":
+          deleteEmployee(connection, start);
+          break;
+        case "Delete Role":
+          deleteRole(connection, start);
+          break;
+        case "Delete Department":
+          deleteDept(connection, start);
+          break;
+        case "Exit":
+          theEnd();
+          break;
+      }
+    });
+}
+const theEnd = () => {
+  console.log("Goodbye and thanks!");
+  connection.end();
 };
 
-function init() {
-  inquirer.prompt(firstQuestion).then((response) => {
-    switch (response.firstQuestion) {
-      case "Add Employee":
-        addEmployee();
-        break;
-      case "Add Department":
-        addDepartment();
-        break;
-      case "Add Role":
-        addRole();
-        break;
-      case "View Employees":
-        viewEmployees();
-        break;
-      case "View By Departments":
-        viewByDepartments();
-        break;
-      case "View By Roles":
-        viewByRoles();
-        break;
-      case "Update Employee Role":
-        updateEmployeeRoles();
-        break;
+const figlett = function () {
+  figlet('Employee Tracker!!', function(err, data) {
+    if (err) {
+        console.log('Something went wrong...');
+        console.dir(err);
+        return;
     }
+    console.log(data)
+    start();
   });
-}
+      
+    
+  };
+ 
+
+function start() {
+  //welcomeScreen();
+  questions();
+} 
